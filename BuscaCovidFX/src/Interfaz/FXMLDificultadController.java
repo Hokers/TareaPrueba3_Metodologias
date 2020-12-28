cf3232602a15e1c779e1de4e6df22bfadb84638e
serllet <fuentesserllet@gmail.com>
2020-12-28 16:21:56 -0300

    Botones con dificultades

--- BuscaCovidFX/src/Interfaz/FXMLDificultadController.java
+++ BuscaCovidFX/src/Interfaz/FXMLDificultadController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Interfaz;
 
 import java.net.URL;
@@ -12,24 +7,22 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-/**
- * FXML Controller class
- *
- * @author Serllet
- */
-public class FXMLDificultadController implements Initializable {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class FXMLDificultadController {
 
     @FXML
-    private Label label;
+    private Button dificultad1;
+
+    @FXML
+    private Button dificultad2;
+
+    @FXML
+    private Button dificultad3;
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
 
     }
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }
-
 }
