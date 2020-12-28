84e558939939b56aa373864b175c28f3db60793d
serllet <fuentesserllet@gmail.com>
2020-12-28 16:10:04 -0300

    detalles interfaz

--- BuscaCovidFX/src/Interfaz/FXMLDificultadController.java
+++ BuscaCovidFX/src/Interfaz/FXMLDificultadController.java
@@ -24,8 +24,7 @@ public class FXMLDificultadController implements Initializable {
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+
     }
 
     @Override
