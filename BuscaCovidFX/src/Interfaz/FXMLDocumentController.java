8501a3b54836f0385439749486c0a2bbb8c78807
Sebastian <sebastian.galdames55@gmail.com>
2020-12-28 23:28:29 -0300

    entralazado de ventanas y tamaño matriz -sebastian

--- BuscaCovidFX/src/Interfaz/FXMLDocumentController.java
+++ BuscaCovidFX/src/Interfaz/FXMLDocumentController.java
@@ -5,15 +5,21 @@
  */
 package Interfaz;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 /**
  *
@@ -27,15 +33,23 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button botonReiniciar;
     
-    int numero=8;
+    static int numero=0;
+    @FXML
+    private AnchorPane anchorPane;
+            
+  
+    
+    double anchoPantalla = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
+    double altoPantalla = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
+  
       
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        for (int i = 0; i < numero; i++) {
-            for (int j = 0; j < numero; j++) {
+        for (int i = 1; i < numero+1; i++) {
+            for (int j = 1; j < numero+1; j++) {
                 ToggleButton nuevoBoton = new ToggleButton(" "); 
-                nuevoBoton.setPrefWidth(40);
-                nuevoBoton.setPrefHeight(40);  
+                nuevoBoton.setPrefWidth(15);
+                nuevoBoton.setPrefHeight(15);  
                 
                 nuevoBoton.setOnAction((ActionEvent events)->{
                     rellenoMatriz();
@@ -44,18 +58,24 @@ public class FXMLDocumentController implements Initializable {
             }
         }
     }    
+    
+    public void inicio() throws IOException{
 
+
+        //Stage stage2 = (Stage) this.inicio.getScene().getWindow(); stage2.close(); 
+        Stage stage = new Stage(); 
+        Parent root2 = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml")); 
+        Scene scene = new Scene(root2); stage.setScene(scene); 
+        stage.show();
+        
+    }
+    
     @FXML
     private void accionReiniciar(ActionEvent event) { 
-        
         
     }
     
     public void rellenoMatriz(){
-       for (int i = 0; i < matrizJuego.getChildren().size(); i++) {
-            System.out.println(i);
-        }
-        
-        
+
     }
 }
